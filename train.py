60a12540327aa4fa004eea1f46e9c63f89b2c64a
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-04-03 13:31:08 -0400

    saving data to google cloud bucket

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
-
-train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], data_dir="gs://thanny/")
+import os
+os.environ["TFDS_TEMP_DIR"] = "gs://thanny/tfds_tmp/"
+train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["mini", "val", "test"], data_dir="gs://thanny/")
