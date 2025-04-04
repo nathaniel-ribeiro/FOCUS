44a625b3aeb44b2dd7de7e05dac527374146de3f
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-04-04 13:05:09 -0400

    added albumentations dependency

--- train.py
+++ train.py
@@ -1,5 +1,13 @@
 import tensorflow_datasets as tfds
 import tensorflow as tf
 import os
+import albumentations as A
+
 os.environ["TFDS_TEMP_DIR"] = "gs://thanny/tfds_tmp/"
-train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["mini", "val", "test"], data_dir="gs://thanny/")
+train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], data_dir="gs://thanny/", as_supervised=True)
+
+train_ds_1 = train_ds.take(1)
+
+for image, label in train_ds_1:
+  print(image.shape)
+  print(type(label))
