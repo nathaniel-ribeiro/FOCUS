720f6aba24f4be93d5c2354081e293fe01279075
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-04-09 15:34:36 -0400

    stubs for get requests

--- server/file_server.py
+++ server/file_server.py
@@ -8,7 +8,7 @@ from PIL import Image
 from io import BytesIO
 
 app = FastAPI()
-train_ds, val_ds, test_ds = None, None, None
+train_ds_numpy, val_ds_numpy, test_ds_numpy = None, None, None
 
 with open('server_config.yaml') as file:
     params = yaml.safe_load(file)
@@ -18,20 +18,33 @@ port = params['port']
 
 @app.on_event("startup")
 def load_dataset():
-    global train_ds, val_ds, test_ds
+    global train_ds_numpy, val_ds_numpy, test_ds_numpy
     train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], data_dir=data_dir, as_supervised=True)
+    train_ds_numpy, val_ds_numpy, test_ds_numpy = train_ds.as_numpy(), val_ds.as_numpy(), test_ds.as_numpy()
+
+#TODO: compress these 3 functions into a single one that takes the index and the name of the partition (["train", "val", "test"])
+#TODO: add a get function that takes the name of the partition to get dataset partition size
 
 @app.get("/train/{idx}")
 def get_training_sample_by_index(idx: int):
-    pass
+    #TODO: find a better way to get the ith example of the dataset without iterating up to it
+    for i, example in enumerate(train_ds_numpy):
+        if i == idx:
+            print(example)
 
 @app.get("/val/{idx}")
 def get_validation_sample_by_index(idx: int):
-    pass
+    #TODO: find a better way to get the ith example of the dataset without iterating up to it
+    for i, example in enumerate(val_ds_numpy):
+        if i == idx:
+            print(example)
 
 @app.get("/test/{idx}")
 def get_test_sample_by_index(idx: int):
-    pass
+    #TODO: find a better way to get the ith example of the dataset without iterating up to it
+    for i, example in enumerate(test_ds_numpy):
+        if i == idx:
+            print(example)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=port)
