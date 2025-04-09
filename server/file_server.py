cb41e00110739bfea166631da707b5a2044bc0e1
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-04-09 13:28:17 -0400

    added data download script

--- server/file_server.py
+++ server/file_server.py
@@ -1,8 +1,13 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import yaml
+
+with open('server_config.yaml') as file:
+    params = yaml.safe_load(file)
 
 def download_dataset():
-
+    data_dir = params[data_dir]
+    train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], data_dir=data_dir, as_supervised=True)
 
 if __name__ == "__main__":
     download_dataset()
