e9e1fdc381d2a044d9e51eddf4afce32c4d7ae59
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-04-26 14:53:33 -0400

    removed file server option

--- train.py
+++ train.py
@@ -3,11 +3,4 @@ import tensorflow as tf
 import os
 import albumentations as A
 
-os.environ["TFDS_TEMP_DIR"] = "gs://thanny/tfds_tmp/"
-train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], data_dir="gs://thanny/", as_supervised=True)
-
-train_ds_1 = train_ds.take(1)
-
-for image, label in train_ds_1:
-  print(image.shape)
-  print(type(label))
+train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], as_supervised=True)
