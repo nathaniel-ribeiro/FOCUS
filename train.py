432be4ec0477cfd7e350630ebda7fabdf4aa1c90
Nathaniel Ribeiro <nathaniel.eldred.ribeiro@gmail.com>
2025-04-26 16:43:48 -0400

    removed as supervised

--- train.py
+++ train.py
@@ -3,4 +3,4 @@ import tensorflow as tf
 import os
 import albumentations as A
 
-train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"], as_supervised=True)
+train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"])
