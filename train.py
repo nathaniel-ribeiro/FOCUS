13d41a22dfb106784539970ec7dfee88ed222d0c
Nathaniel Ribeiro <nathaniel.eldred.ribeiro@gmail.com>
2025-05-20 14:04:58 -0400

    removed old copies of files after moving

--- train.py
+++
@@ -1,6 +0,0 @@
-import tensorflow_datasets as tfds
-import tensorflow as tf
-import os
-import albumentations as A
-
-train_ds, val_ds, test_ds = tfds.load(name="i_naturalist2021", split=["train", "val", "test"])
