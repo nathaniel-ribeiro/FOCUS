fe1c7603c52c1780cfb5093033f603c1f6064e07
Nathaniel Ribeiro <nathaniel.eldred.ribeiro@gmail.com>
2025-05-21 19:46:23 -0400

    completed dataloader

--- training/inat_dataloader.py
+++ training/inat_dataloader.py
@@ -1,17 +1,34 @@
 import torch
-import yaml
 from PIL import Image
 import os
 import json
 import random
 import numpy
+import yaml
 
 with open('config.yaml') as f:
     params = yaml.safe_load(f)
-
 data_dir = params['data_dir']
 
+categories_filepath = os.path.join(data_dir, 'categories.json')
+with open(categories_filepath) as f:
+    categories = json.load(f)
+
 class INaturalistDataset(torch.utils.data.Dataset):
-    def __init__(self, transforms):
+    def __init__(self, root_directory, annotations_filepath, augmentations):
+        with open(annotations_filepath) as f:
+            annotations_data = json.load(f)
+        self.image_filenames = [annotation['file_name'] for annotation in annotations_data]
+        self.ids = [annotation['category_id'] for annotation in annotations_data]
         
+    def __getitem__(self, idx):
+        path = root_directory + self.image_filenames[idx]
+        image_id = self.ids[idx]
+        image_taxonomy = categories[image_id]
+        image = Image.open(path).convert('RGB')
+        augmented_image = augmentations(image)
+        return augmented_image, image_id, image_taxonomy
+    
+    def __len__(self):
+        return len(self.image_filenames)
         
