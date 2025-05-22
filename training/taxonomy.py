5c5b561e6a1a64621f0fdacd7f5f8a57d3aa7984
Nathaniel Ribeiro <nathaniel.eldred.ribeiro@gmail.com>
2025-05-21 21:41:22 -0400

    created class to "wrap" the categories

---
+++ training/taxonomy.py
@@ -0,0 +1,11 @@
+class Taxonomy:
+    def __init__(self, kingdom, name, family, supercategory, phylum, taxon_id, order, genus, taxon_class)
+        self.kingdom = kingdom
+        self.name = name
+        self.family = family
+        self.supercategory = supercategory
+        self.phylum = phylum
+        self.taxon_id = taxon_id
+        self.order = order
+        self.genus = genus
+        self.taxon_class = taxon_class
