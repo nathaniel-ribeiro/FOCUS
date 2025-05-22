bba8d308cb09368558174eeae5640c0bbd4703d7
Nathaniel Ribeiro <nathaniel.eldred.ribeiro@gmail.com>
2025-05-22 10:01:07 -0400

    loading config file returns a data class rather than a dict

--- training/utils.py
+++ training/utils.py
@@ -1,5 +1,14 @@
 import yaml
+from dataclasses import dataclass
+
+@dataclass
+class TrainingOptions:
+    data_dir: str
+    target_size: tuple
+    batch_size: int
+    num_workers: int
 
 def load_config_file(path):
     with open(path) as f:
-        return yaml.safe_load(f)
+        params = yaml.safe_load(f)
+    return TrainingOptions(**params)
