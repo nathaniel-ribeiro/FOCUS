b025e1ee5e135807fec6cb53567ba63d2ba53aa9
Nathaniel Ribeiro <cwa6zf@virginia.edu>
2025-12-09 19:46:02 -0500

    removed irrelevant templates

--- training/micronet_data.py
+++ training/micronet_data.py
@@ -11,6 +11,7 @@ from taxonomy import Taxonomy
 from utils import load_config_file, load_micronet_metadata
 import torch.multiprocessing as mp
 import albumentations as A
+import math
 
 options = load_config_file('config.yaml')
 data_dir = options.data_dir
@@ -26,8 +27,6 @@ NUM_WORKERS = min(mp.cpu_count() - 1, 8)
 # NOTE: OpenCLIP normalization is replicated, skip OpenCLIP's built in preprocessor
 OPENCLIP_MEAN = [0.48145466, 0.4578275, 0.40821073]
 OPENCLIP_STD  = [0.26862954, 0.26130258, 0.27577711]
-
-BRIGHTNESS, CONTRAST, SATURATION, HUE = 0.4, 0.4, 0.4, 0.25
 
 train_transforms = A.Compose([
     A.RandomResizedCrop(size=TARGET_SIZE),
@@ -46,84 +45,43 @@ test_transforms = A.Compose([
 imagenet_templates = [
     'A bad photo of a {label}.',
     'A photo of many {label}.',
-    'A sculpture of a {label}.',
     'A photo of the hard to see {label}.',
     'A low resolution photo of the {label}.',
-    'A rendering of a {label}.',
-    'Graffiti of a {label}.',
     'A bad photo of the {label}.',
     'A cropped photo of the {label}.',
-    'A tattoo of a {label}.',
-    'The embroidered {label}.',
     'A photo of a hard to see {label}.',
     'A bright photo of a {label}.',
     'A photo of a clean {label}.',
     'A photo of a dirty {label}.',
     'A dark photo of the {label}.',
-    'A drawing of a {label}.',
     'A photo of my {label}.',
-    'The plastic {label}.',
     'A photo of the cool {label}.',
     'A close-up photo of a {label}.',
     'A black and white photo of the {label}.',
-    'A painting of the {label}.',
-    'A painting of a {label}.',
     'A pixelated photo of the {label}.',
-    'A sculpture of the {label}.',
     'A bright photo of the {label}.',
     'A cropped photo of a {label}.',
-    'A plastic {label}.',
     'A photo of the dirty {label}.',
     'A JPEG corrupted photo of a {label}.',
     'A blurry photo of the {label}.',
     'A photo of the {label}.',
     'A good photo of the {label}.',
-    'A rendering of the {label}.',
-    'A {label} in a video game.',
-    'A photo of one {label}.',
-    'A doodle of a {label}.',
     'A close-up photo of the {label}.',
     'A photo of a {label}.',
-    'The origami {label}.',
-    'The {label} in a video game.',
-    'A sketch of a {label}.',
-    'A doodle of the {label}.',
     'A origami {label}.',
     'A low resolution photo of a {label}.',
-    'The toy {label}.',
-    'A rendition of the {label}.',
-    'A photo of the clean {label}.',
-    'A photo of a large {label}.',
-    'A rendition of a {label}.',
     'A photo of a nice {label}.',
     'A photo of a weird {label}.',
     'A blurry photo of a {label}.',
-    'A cartoon {label}.',
-    'Art of a {label}.',
-    'A sketch of the {label}.',
-    'A embroidered {label}.',
     'A pixelated photo of a {label}.',
-    'ITAP of the {label}.',
     'A JPEG corrupted photo of the {label}.',
     'A good photo of a {label}.',
-    'A plushie {label}.',
     'A photo of the nice {label}.',
     'A photo of the small {label}.',
     'A photo of the weird {label}.',
-    'The cartoon {label}.',
-    'Art of the {label}.',
-    'A drawing of the {label}.',
-    'A photo of the large {label}.',
-    'A black and white photo of a {label}.',
-    'The plushie {label}.',
     'A dark photo of a {label}.',
-    'ITAP of a {label}.',
-    'Graffiti of the {label}.',
-    'A toy {label}.',
-    'ITAP of my {label}.',
     'A photo of a cool {label}.',
     'A photo of a small {label}.',
-    'A tattoo of the {label}.',
 ]
 
 class MicroNetDataset(data.Dataset):
@@ -144,31 +102,44 @@ class MicroNetDataset(data.Dataset):
     
     def __getitem__(self, idx):
         image_id = self.ids[idx]
-        path = os.path.join(data_dir, "micronet_images", image_id)
+        path = os.path.join(data_dir, self.images_directory, f'{image_id}.jpg')
         image_metadata = metadata[image_id]
         scientific_name = image_metadata['scientific_name']
+
+        if(scientific_name is None):
+            raise ValueError("The requested image does not have a scientific name associated.")
+        
         image_prompt = self.get_prompt(scientific_name)
         image = Image.open(path).convert('RGB')
         image_numpy = np.array(image)
         augmented_image = self.augmentations(image=image_numpy)['image']
 
-        category_id = int(image_metadata['taxon_id'])
+        category_id = -1 if math.isnan(image_metadata['taxon_id']) else int(image_metadata['taxon_id'])
         return augmented_image, category_id, image_prompt
     
     def __len__(self):
         return len(self.ids)
 
 def make_datasets():   
-    train_dataset = MicroNetDataset(train_root_dir, train_annotations_filepath, train_transforms, True)
-    test_dataset = MicroNetDataset(test_root_dir, test_annotations_filepath, test_transforms, False)
-    return train_dataset, test_dataset
+    train_dataset = MicroNetDataset(os.path.join(data_dir, "train.json"), "micronet_images", train_transforms, True)
+    val_dataset = MicroNetDataset(os.path.join(data_dir, "val.json"), "micronet_images", test_transforms, False)
+    test_dataset = MicroNetDataset(os.path.join(data_dir, "test.json"), "micronet_images", test_transforms, False)
+    return train_dataset, val_dataset, test_dataset
 
 def make_dataloaders():
-    train_dataset, test_dataset = make_datasets()
+    train_dataset, val_dataset, test_dataset = make_datasets()
     train_loader = data.DataLoader(train_dataset, BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
+    val_loader = data.DataLoader(val_dataset, BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
     test_loader = data.DataLoader(test_dataset, BATCH_SIZE, shuffle=False, num_workers=NUM_WORKERS)
-    return train_loader, test_loader
+    return train_loader, val_loader, test_loader
 
 def get_labels():
     labels = [item['scientific_name'] for item in metadata]
+    labels = list(set(labels))
     return labels
+
+if __name__ == "__main__":
+    train_loader, val_loader, test_loader = make_dataloaders()
+    for images, ids, prompts in train_loader:
+        print(prompts)
+        break
